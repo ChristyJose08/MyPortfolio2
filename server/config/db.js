8dc8d2b38397d939306e9c8f86a021343ff3f333
ChristyJose08 <cjose8@my.centennialcollege.ca>
2023-02-17 23:28:40 -0500

    version 1

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports = 
+{
+    // "URI" : "mongodb://127.0.0.1:27017/businessContact",
+   "URI": "mongodb+srv://christy:8qVLKeiFz82LwNF6@mongodbserver.bi8hsof.mongodb.net/business_contact",
+   "Secret": 'SomeSecret'
+}
+
